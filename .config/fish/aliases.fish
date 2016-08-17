edea1a3d263caa46fe8945a28d7591fb4131495e
Joshua Miles <milesjos@msu.edu>
2016-08-17 13:05:51 -0400

    Add Fish/omf stuff

---
+++ .config/fish/aliases.fish
@@ -0,0 +1,21 @@
+# ..
+alias .... "cd ../.."
+alias ...... "cd ../../.."
+
+# git
+alias gst "git status"
+alias ga "git add"
+alias gaa "git add --all"
+alias gcmsg "git commit -m"
+alias gp "git push"
+alias gf "git fetch"
+alias gdf "git diff"
+
+# rails
+alias rails_db_reset "rake db:drop; and rake db:create; and rake db:migrate"
+alias be "bundle exec"
+alias rs "rails s"
+alias rc "rails c"
+
+# macvim
+alias vim "mvim -v"
